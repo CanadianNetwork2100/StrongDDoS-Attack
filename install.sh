94d43a9b62ad554f6bc99d1586105b8376d7c025
Ty Ridsdill <147262313+CanadianNetwork2030@users.noreply.github.com>
2023-10-09 14:21:31 -0400

    install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+apk add figlet -y
+clear
+figlet "StrongDDos"
+echo "\n"
+echo "[*] Started installing StrongDDos script!"
+echo "\n"
+apk add python2 python3
+clear
+figlet "All Packages Successfully installed"
+echo "[*] Now you can run script with command 'python2 script.py'"
